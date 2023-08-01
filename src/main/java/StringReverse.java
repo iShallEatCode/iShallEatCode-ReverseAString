16198d49c6c4c44d660181101ea78f11960f54e9
Artem Synytsia <synytsia.artem@yahoo.com>
2023-08-01 01:22:22 +0000

    WkNine_DayFour_LabOne

--- src/main/java/StringReverse.java
+++ src/main/java/StringReverse.java
@@ -13,6 +13,11 @@ public class StringReverse {
      * @return the reverse of str.
      */
     public String reverse(String str){
-        return null;
+
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+
+        return new StringBuilder(str).reverse().toString();
     }
 }
